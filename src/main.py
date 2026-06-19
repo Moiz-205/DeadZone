057ffd7ba30304692e5c28ee0c051360bb20e932
Moiz-205 <moizrajput205@gmail.com>
2026-06-19 22:54:41 +0500

    main updated with date picker utility function

--- src/main.py
+++ src/main.py
@@ -10,19 +10,35 @@ def main(page: ft.Page):
     page.bgcolor = BG_COLOR
 
     ## Interface components
-    input_field = dialog.create_input_field()
+    title_field = dialog.create_title_field()
+    start_date_field, start_date_picker = dialog.create_start_date_field()
+    end_date_field, end_date_picker = dialog.create_end_date_field()
+
     tasks_column = layout.create_tasks_layout(page, tasks=data.get_tasks())
 
     ## Button handlers components
     cancel_handler = handlers.cancel_dialog_button(page)
-    add_handler = handlers.add_dialog_button(page, input_field, tasks_column, layout)
+    add_handler = handlers.add_dialog_button(
+        page,
+        title_field,
+        start_date_field,
+        end_date_field,
+        tasks_column,
+        layout)
     fab_handler = handlers.open_dialog_button(page)
 
     ## Overlay componet
-    input_dialog = dialog.create_input_dialog(input_field, cancel_handler, add_handler)
+    input_dialog = dialog.create_input_dialog(
+        title_field,
+        start_date_field,
+        end_date_field,
+        cancel_handler,
+        add_handler
+    )
 
     ## Main screen
-    page.overlay.append(input_dialog)
+    # page.overlay.append(input_dialog)
+    page.overlay.extend([input_dialog, start_date_picker, end_date_picker])
     page.floating_action_button = buttons.create_fab_button(fab_handler)
     page.add(tasks_column)
 
