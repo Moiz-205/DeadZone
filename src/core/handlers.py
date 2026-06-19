3395c1dca6cd307e8b4911b1ea20d5a4959138a5
Moiz-205 <moizrajput205@gmail.com>
2026-06-19 22:53:09 +0500

    date picker feature add

--- src/core/handlers.py
+++ src/core/handlers.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from core import data
 
 def cancel_dialog_button(page):
@@ -7,18 +8,32 @@ def cancel_dialog_button(page):
         page.update()
     return handler
 
-def add_dialog_button(page, input_field, tasks_column, layout):
+def add_dialog_button(page,
+    title_field,
+    start_date_field,
+    end_date_field,
+    tasks_column,
+    layout):
     def handler(e):
-        task_title = input_field.value
-        start_date = "2026-12-25"
-        end_date = "2026-12-31"
-        data.add_task(task_title, start_date, end_date)
+        task_title = title_field.value
+        start_date_str = start_date_field.value
+        end_date_str = end_date_field.value
 
+        try:
+            start_date = datetime.strptime(start_date_str, "%Y-%m-%d").date()
+            end_date = datetime.strptime(end_date_str, "%Y-%m-%d").date()
+        except ValueError:
+            print("Invalid date format.")
+            return
+
+        data.add_task(task_title, start_date, end_date)
         layout.render_tasks(page, data.get_tasks(), tasks_column)
 
         if page.overlay:
             page.overlay[0].open = False
-        input_field.value = ""
+        title_field.value = ""
+        start_date_field.value = ""
+        end_date_field.value = ""
         page.update()
     return handler
 
